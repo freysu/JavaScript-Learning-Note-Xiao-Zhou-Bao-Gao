9e7747235d1a288ed5759a24cde7bb600b9be76f
freysu <945806165@qq.com>
2021-08-20 02:05:07 +0800

    2021-8-20-2:05

---
+++ JS/test.js
@@ -0,0 +1,11 @@
+function fun(n, m) {
+    return n + m;
+}
+let result1 = fun(10, 20);
+let result2 = fun(100, 200);
+console.log(result1);
+console.log(result2);
+// 函数声明提升
+function fun(n, m) {
+    return n + m;
+}
